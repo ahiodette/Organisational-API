0630fc3a36ddd5e167d24cada8c8e10228cd0210
ahiodette <ahiode6@gmail.com>
2019-10-01 19:34:23 +0200

    Add paths in App.java file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,20 @@
-public class App {
+import spark.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+    }
 }
